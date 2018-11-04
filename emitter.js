8a645cc6ef124c1d913f4705a27fa4e0e16bbbad
Liza Ginzburg <lizagin98@gmail.com>
2018-11-04 19:29:55 +0500

    first

--- emitter.js
+++ emitter.js
@@ -4,13 +4,16 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+
+    const events = new Map();
+
     return {
 
         /**
@@ -18,26 +21,66 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (!events.has(event)) {
+                events.set(event, new Map([[context, [handler]]]));
+
+                return this;
+            }
+            if (!events.get(event).has(context)) {
+                events.get(event).set(context, [handler]);
+
+                return this;
+            }
+
+            events.get(event).get(context)
+                .push(handler);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            events.forEach((value, key) => {
+                if (key.startsWith(event + '.') || key === event) {
+                    value.delete(context);
+                }
+            });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            function allEvents(e) {
+                const splitEvent = e.split('.');
+                let newEvents = [];
+                for (let i = 0; i < splitEvent.length; i++) {
+                    newEvents.push((newEvents.length === 0)
+                        ? splitEvent[i] : [newEvents[i - 1], splitEvent[i]].join('.'));
+                }
+
+                return newEvents.reverse();
+            }
+            const newEvents = allEvents(event).filter(emitEvent =>
+                events.has(emitEvent));
+
+            newEvents.forEach(emitEvent =>
+                events.get(emitEvent).forEach((handlers, context) =>
+                    handlers.forEach(handler => handler.call(context))));
+
+            return this;
         },
 
         /**
